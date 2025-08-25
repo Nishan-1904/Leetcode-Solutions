537c243455cdaafe838f6070058c3b31e334397e
Nishan Chakraborty <141174315+Nishan-1904@users.noreply.github.com>
2025-08-26 00:05:07 +0530

    Time: 27 ms (26.59%) | Memory: 17.6 MB (27.93%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> result;
         int n = nums.size();
-        if (n < 4) return result;
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < n - 3; ++i) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            for (int j = i + 1; j < n - 2; ++j) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-                int left = j + 1, right = n - 1;
-                while (left < right) {
-                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
-                    if (sum == target) {
-                        result.push_back({nums[i], nums[j], nums[left], nums[right]});
-                        while (left < right && nums[left] == nums[left + 1]) ++left;
-                        while (left < right && nums[right] == nums[right - 1]) --right;
-                        ++left;
-                        --right;
-                    } else if (sum < target) {
-                        ++left;
-                    } else {
-                        --right;
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n;j++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+                int k = j+1;
+                int l = n-1;
+                while(k<l){
+                    long long sum = (long long) nums[i]+nums[j]+nums[k]+nums[l];
+                    if(sum<target) k++;
+                    else if(sum>target) l--;
+                    else{
+                        vector<int> temp = {nums[i],nums[j],nums[k],nums[l]};
+                        ans.push_back(temp);
+                        k++;
+                        l--;
+                        while(k<l && nums[k]==nums[k-1]) k++;
+                        while(k<l && nums[l]==nums[l+1]) l--;
                     }
                 }
             }
         }
-        return result;
+        return ans;
     }
 };
